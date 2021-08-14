19ad59c31767d692cace2b50bf7a11b9c5001f71
KristinaErdman <erdmakristina8@gmail.com>
2021-08-14 22:08:59 +0700

    завершен п.3b

--- server/apps/money/serializers.py
+++ server/apps/money/serializers.py
@@ -1,42 +1,35 @@
-from django.db import models
-from django.db.models.functions import Coalesce
-from rest_framework.serializers import (Serializer, ModelSerializer,
-                                        SerializerMethodField, DecimalField)
+from rest_framework import serializers
 from .models import Category, Transaction
 
 
-class CategorySerializer(ModelSerializer):
+class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ('pk', 'type', 'name', 'owner', )
 
 
-class CategoryListSerializer(ModelSerializer):
-    sum_amount = SerializerMethodField()
-
-    class Meta:
-        model = Category
-        fields = ('pk', 'type', 'name', 'owner', 'sum_amount', )
-
-    def get_sum_amount(self, obj):
-        res = obj.transactions.aggregate(sum=Coalesce(models.Sum('amount'), 0.0,
-                                                      output_field=models.DecimalField()))
-        return res['sum']
+class CategoryListSerializer(serializers.Serializer):
+    category = serializers.IntegerField()
+    pk = serializers.IntegerField()
+    type = serializers.CharField()
+    name = serializers.CharField()
+    owner = serializers.IntegerField()
+    sum_amount = serializers.DecimalField(read_only=True, max_digits=12, decimal_places=2)
 
 
-class CategorySumByTypeSerializer(Serializer):
-    income = DecimalField(read_only=True, max_digits=12, decimal_places=2)
-    expense = DecimalField(read_only=True, max_digits=12, decimal_places=2)
+class CategorySumByTypeSerializer(serializers.Serializer):
+    income = serializers.DecimalField(read_only=True, max_digits=12, decimal_places=2)
+    expense = serializers.DecimalField(read_only=True, max_digits=12, decimal_places=2)
 
 
-class TransactionSerializer(ModelSerializer):
+class TransactionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Transaction
         fields = ('pk', 'owner', 'category', 'amount', 'date', )
 
 
-class TransactionListSerializer(ModelSerializer):
-    type = SerializerMethodField()
+class TransactionListSerializer(serializers.ModelSerializer):
+    type = serializers.SerializerMethodField()
 
     class Meta:
         model = Transaction
