7669a4d69a599ea5b6b2787c5d9a359c30817602
KristinaErdman <erdmakristina8@gmail.com>
2021-08-19 15:45:25 +0700

    исправлено 2. удалены комментарии

--- server/apps/money/tests/__init__.py
+++ server/apps/money/tests/__init__.py
@@ -4,6 +4,6 @@ from .get_category_trs_or_widgets import (GetCategoryTransactionWidgetFromUser,
                                          GetCategoryTransactionWidgetFromAnonym)
 from .post_category_trs_or_widget import (PostCategoryTransactionWidgetWithValidToken,
                                           PostCategoryTransactionWidgetWithInvalidToken)
-# from .update_or_delete_transaction import (UpdateOrDeleteTransactionFromUser,
-#                                           UpdateOrDeleteTransactionFromAnonym)
+from .update_or_delete_transaction import (UpdateOrDeleteTransactionFromUser,
+                                          UpdateOrDeleteTransactionFromAnonym)
 from .factory_data import *
