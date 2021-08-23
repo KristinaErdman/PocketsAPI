2f58394e5a9d3c87379b1c208f2d9cfcdd4f18e3
KristinaErdman <erdmakristina8@gmail.com>
2021-08-23 15:46:45 +0700

    задание 9: добавлены изменения в маршруты

--- server/apps/users/urls.py
+++ server/apps/users/urls.py
@@ -1,11 +1,14 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import UserRegistrationView, UserInfoView
 
-
-urlpatterns = [
+auth_urlpatterns = [
     path('sign_up/', UserRegistrationView.as_view(), name='sign_up'),
     path('sign_in/', TokenObtainPairView.as_view(), name='sign_in'),  # получает пару токенов
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  # обновляет токен
+]
+
+urlpatterns = [
+    path('auth/', include(auth_urlpatterns), name='auth'),
     path('me/', UserInfoView.as_view(), name='me'),
 ]
